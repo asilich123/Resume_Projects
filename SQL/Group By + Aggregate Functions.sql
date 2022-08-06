50456b12abc2990c56ba84ecdcc9611921121894
Adrian <asilich123@gmail.com>
2022-08-06 12:58:13 -0700

    Foundation of SQL Directory

---
+++ SQL/Group By + Aggregate Functions.sql
@@ -0,0 +1,11 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT 
+	COUNT(a.[PID]) as NumOfOffers, 
+	b.[PCode], 
+	AVG(a.[OfferPrice]) / AVG(a.[ListedPrice]) as AvgOfferPrctofListP
+
+  FROM [PERLESMP4].[dbo].[tblPropertyOffers] a
+  INNER JOIN [PERLESMP4].[dbo].[tblProperty] b
+  ON a.PID = b.PID
+  GROUP BY a.[PID], b.[PCode]
+  HAVING MIN(a.[OfferReceived]) >= '2022-04-01'
